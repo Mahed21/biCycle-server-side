69adba2f0758d95af38833af946588a1d4684113
Mahed21 <mahed255261@gmail.com>
2021-11-13 00:47:56 +0600

    second commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors=require('cors');
 const ObjectId=require('mongodb').ObjectId;
 require('dotenv').config()
 const app = express();
-const port = 5000
+const port = process.env.PORT || 5000
 app.use(cors());
 app.use(express.json());
 
@@ -18,6 +18,9 @@ async function run() {
     await client.connect();
     const database = client.db("cycle");
     const userCollection = database.collection("user");
+    const clientCollection = database.collection("client");
+    const rivewCollection = database.collection("rivew");
+    const loginCollection=database.collection("users");
   
    //get api
    app.get('/products',async (req,res)=>
@@ -26,6 +29,92 @@ async function run() {
      const users=await cursor.toArray();
      res.send(users);
    })
+   //load single item
+   app.get('/products/:id', async (req,res)=>
+   {
+     const id =req.params.id;
+     const query={_id: ObjectId(id)};
+     const user =await userCollection.findOne(query);
+     res.send(user);
+   })
+   //post api
+   app.post('/rivew', async (req,res)=>
+   {
+     console.log('hi');
+     const newUser = req.body;
+     const result = await rivewCollection.insertOne(newUser);
+      console.log('hitting the post',req.body);
+      res.json(result);
+   })
+   //post api
+   app.post('/client', async (req,res)=>
+   {
+     const newUser = req.body;
+     const result = await clientCollection.insertOne(newUser);
+      console.log('hitting the post',req.body);
+      res.json(result);
+   })
+   //post api
+  //  app.post('/users', async (req,res)=>
+  //  {
+  //    const newUser = req.body;
+  //    const result = await loginCollection.insertOne(newUser);
+  //     console.log('hitting the post',req.body);
+  //     res.json(result);
+  //  })
+   //get reviw
+   app.get('/rivew', async (req,res)=>
+   {
+    const cursor=rivewCollection.find ({})
+     const users=await cursor.toArray();
+     res.send(users);
+   })
+   //get users
+   app.get('/users', async (req,res)=>
+   {
+    const cursor=loginCollection.find ({})
+     const users=await cursor.toArray();
+     res.send(users);
+   })
+   //get client order
+   app.get('/client', async (req,res)=>
+   {
+    const cursor=clientCollection.find ({})
+     const users=await cursor.toArray();
+     res.send(users);
+   })
+
+   //get by email
+   app.get('/client/:email' , async (req,res)=>
+   {
+     const userEmail =req.params.email;
+     const query ={email:{$in :[userEmail]}};
+   
+     const result =await clientCollection.find(query).toArray();
+     res.json(result);
+     
+   })
+   //delete
+   app.delete('/client/:id' , async (req,res)=>
+   {
+     const id =req.params.id;
+     const query ={_id : ObjectId(id)};
+     const result =await clientCollection.deleteOne(query);
+     res.json(result);
+     
+   })
+  //  app.put('/users/admin',async (req,res)=>
+  //  {
+  //    const user=req.body;
+  //    console.log(user)
+  //    const filter={email: user.email}
+  //    const updateDoc= {$set:{role:'admin'}};
+  //    const result=await loginCollection.updateOne(filter,updateDoc);
+  //    res.json(result);
+  //  })
+   
+
+  
 
    
 
